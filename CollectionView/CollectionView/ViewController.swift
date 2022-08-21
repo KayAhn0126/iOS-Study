e20fbf60ddf0fb30fb8337ba64e9cdaae7ec9b23
KayAhn0126 <dkseotn123@gmail.com>
2022-08-21 18:03:15 +0900

    feat: CollectionView 추가

---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Kay on 2022/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
