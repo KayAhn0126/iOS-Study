79175a78da502d4d357f66ee90160d5eec3f4ec3
KayAhn0126 <dkseotn123@gmail.com>
2022-08-22 12:27:54 +0900

    feat: ScreenTransition 프로젝트 추가

---
+++ ScreenTransition/ScreenTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransition
+//
+//  Created by Kay on 2022/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
