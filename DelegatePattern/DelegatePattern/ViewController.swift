ecdb6c203250145d728d54c7476545209df0c933
KayAhn0126 <dkseotn123@gmail.com>
2022-08-24 19:02:18 +0900

    refactor: Delegation Pattern 연습 추가

--- DelegatePattern/DelegatePattern/ViewController.swift
+++ DelegatePattern/DelegatePattern/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+protocol PrepareParty: AnyObject {
+    func prepareFood()
+    func prepareSong()
 }
 
+class PartyDirector {
+    var delegate: PrepareParty?
+    
+    func order() {
+        self.delegate?.prepareFood() // 내가 위임한 사람(대리자)의 prepareFood()를 실행해라
+        self.delegate?.prepareSong()
+    }
+}
+
+class FirstPartyWorker: PrepareParty {
+    init(director: PartyDirector) {
+        director.delegate = self
+    }
+    func prepareFood() {
+        print("first worker is preparing food!")
+    }
+    func prepareSong() {
+        print("first worker will sing a song!")
+    }
+}
+
+
+class SecondPartyWorker: PrepareParty {
+    init(director: PartyDirector) {
+        director.delegate = self
+    }
+    func prepareFood() {
+        print("second worker is preparing food!")
+    }
+    func prepareSong() {
+        print("second worker will sing a song!")
+    }
+}
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let Kay = PartyDirector()
+        let worker1 = FirstPartyWorker(director: Kay)
+        Kay.order()
+        let worker2 = SecondPartyWorker(director: Kay)
+        Kay.order()
+    }
+}
