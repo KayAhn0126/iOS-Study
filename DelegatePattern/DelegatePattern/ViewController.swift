c0bb4eb21a21c682ef0456b74ba4ec89284ec1a2
KayAhn0126 <dkseotn123@gmail.com>
2022-08-24 16:19:36 +0900

    feat: Delegate Pattern 프로젝트 추가

---
+++ DelegatePattern/DelegatePattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegatePattern
+//
+//  Created by Kay on 2022/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
