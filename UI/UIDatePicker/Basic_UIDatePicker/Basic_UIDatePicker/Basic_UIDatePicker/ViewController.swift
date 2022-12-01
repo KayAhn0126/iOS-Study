a7c256bec0b4989701278aa94e32ad55eee015e1
KayAhn0126 <dkseotn123@gmail.com>
2022-12-01 23:56:20 +0900

    refactor: 테스트를 위한 코드 작성

--- UI/UIDatePicker/Basic_UIDatePicker/Basic_UIDatePicker/Basic_UIDatePicker/ViewController.swift
+++ UI/UIDatePicker/Basic_UIDatePicker/Basic_UIDatePicker/Basic_UIDatePicker/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let datePicker = UIDatePicker()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
+    // MARK: - UI
+    private func configureUI() {
+        setAttributes()
+        setContraints()
+    }
 
+    private func setAttributes() {
+        datePicker.backgroundColor = .green
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .dateAndTime
+        datePicker.locale = Locale(identifier: "ko-KR")
+        datePicker.timeZone = .autoupdatingCurrent
+        datePicker.minuteInterval = 15
+        datePicker.addTarget(self, action: #selector(handleDatePicker(_:)), for: .valueChanged)
+    }
+
+    private func setContraints() {
+        view.addSubview(datePicker)
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+
+    // MARK: - Selectors
+    @objc
+    private func handleDatePicker(_ sender: UIDatePicker) {
+        print(sender.date)
+    }
 }
 
