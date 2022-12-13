8c7f31de5e4f076d8cd77ee878a9120ffbd6cd5b
KayAhn0126 <dkseotn123@gmail.com>
2022-12-13 20:26:07 +0900

    feat: 열거형 테스트를 위한 파일 추가

---
+++ GrammarAndKnowledge/Enumeration/Enumertaion/Enumertaion/AppleProductEnum.swift
@@ -0,0 +1,8 @@
+//
+//  AppleProductEnum.swift
+//  Enumertaion
+//
+//  Created by Kay on 2022/12/13.
+//
+
+import Foundation
---
+++ GrammarAndKnowledge/Enumeration/Enumertaion/Enumertaion/Storage.swift
@@ -0,0 +1,8 @@
+//
+//  Storage.swift
+//  Enumertaion
+//
+//  Created by Kay on 2022/12/13.
+//
+
+import Foundation
